70401327e25c9a88f5d1d2d801d0fa86d9180d20
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-01-05 16:38:52 +0530

    Update tst.c

--- tst.c
+++ tst.c
@@ -1,7 +1,23 @@
-#include<['/stdio.h>
-void main(){
-    int x;
-    x=10,20,30;
-    printf("%d",x);
-   return 0;
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the value: \n");
+	scanf("%d",&a);
+	printf("\n the value is:%d",a);
+	if(a>0)
+	{
+		printf("\n the value is positive");
+	}
+	else if(a<0)
+	{
+		printf("the value is negative \n");
 }
+
+		else
+{
+		printf("the value is zero \n");
+return 0;
+	}
+	}
+	
